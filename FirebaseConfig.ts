1bcd644757093459af7a35cc89c9b8faea736221
sanfo <sanfo.bt@gmail.com>
2025-06-06 16:31:20 +0800

    set up .env

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,21 +1,28 @@
-
-
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
 //@ts-ignore
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjl1aYBZ_IfT36fMTuiVJGo5LIPnS0B6o",
-  authDomain: "owetest-edfc7.firebaseapp.com",
-  projectId: "owetest-edfc7",
-  storageBucket: "owetest-edfc7.firebasestorage.app",
-  messagingSenderId: "783199309926",
-  appId: "1:783199309926:web:d8e325893bbaf4a18a9da7",
-  measurementId: "G-08T2YW5MB9"
-  };
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
 
 const FIREBASE_APP = initializeApp(firebaseConfig);
 
