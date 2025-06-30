b56011543dcaed3040b5c21f6d34eedea04d5b6d
Chellappan Ramiah <151884326+Chellu19@users.noreply.github.com>
2025-06-30 09:58:08 +0800

    Update Insights.tsx

--- app/screens/Insights.tsx
+++ app/screens/Insights.tsx
@@ -4,7 +4,7 @@ import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
 
 const MODEL_URL =
-  'https://expensemodel-m6aug6g31-chellu19s-projects.vercel.app';
+  'https://expensemodel-m6aug6g31-chellu19s-projects.vercel.app/model.json';
 
 const InsightsScreen = () => {
   const [isReady, setIsReady] = useState(false);
