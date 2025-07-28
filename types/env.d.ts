4f21de93ded416b420b9971cd8b4f4a96fe98522
sanfo <sanfo.bt@gmail.com>
2025-07-28 10:51:39 +0800

    model loading

--- types/env.d.ts
+++ types/env.d.ts
@@ -5,5 +5,6 @@ declare module '@env' {
     export const FIREBASE_STORAGE_BUCKET: string;
     export const FIREBASE_MESSAGING_SENDER_ID: string;
     export const FIREBASE_APP_ID: string;   
-    export const FIREBASE_MEASUREMENT_ID: string;   
+    export const FIREBASE_MEASUREMENT_ID: string;  
+    export const MY_API_KEY: string; 
   }
